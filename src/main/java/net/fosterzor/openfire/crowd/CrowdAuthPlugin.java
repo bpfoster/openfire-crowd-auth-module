88fa78e7241295f1daa25a267d6f01348c5dfe6f
Ben Foster <bpfoster@gmail.com>
2011-05-25 14:36:59 -0400

    Add plugin

---
+++ src/main/java/net/fosterzor/openfire/crowd/CrowdAuthPlugin.java
@@ -0,0 +1,31 @@
+package net.fosterzor.openfire.crowd;
+
+
+import org.jivesoftware.openfire.container.Plugin;
+import org.jivesoftware.openfire.container.PluginManager;
+
+import java.io.File;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: bpfoster
+ * Date: 5/25/11
+ * Time: 2:32 PM
+ * To change this template use File | Settings | File Templates.
+ * http://www.igniterealtime.org/builds/openfire/docs/latest/documentation/plugin-dev-guide.html
+ */
+public class CrowdAuthPlugin implements Plugin {
+    //JiveGlobals.getProperty(String)
+    
+    @Override
+    public void initializePlugin(PluginManager manager, File pluginDirectory) {
+        // TODO: Auto-generated method
+
+    }
+
+    @Override
+    public void destroyPlugin() {
+        // TODO: Auto-generated method
+
+    }
+}
