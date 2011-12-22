487dec0811b7606a6bd9cda3df43bb0b2233e990
bfoster <benjamin.foster@six3systems.com>
2011-12-22 13:41:13 -0500

    Configurable crowd properties

--- src/main/java/net/fosterzor/openfire/crowd/CrowdClientHolder.java
+++ src/main/java/net/fosterzor/openfire/crowd/CrowdClientHolder.java
@@ -2,6 +2,12 @@ package net.fosterzor.openfire.crowd;
 
 import com.atlassian.crowd.integration.rest.service.factory.RestCrowdClientFactory;
 import com.atlassian.crowd.service.client.CrowdClient;
+import org.jivesoftware.util.JiveGlobals;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.util.Properties;
 
 /**
  * Created by IntelliJ IDEA.
@@ -11,15 +17,41 @@ import com.atlassian.crowd.service.client.CrowdClient;
  * To change this template use File | Settings | File Templates.
  */
 public class CrowdClientHolder {
-    private static CrowdClient client;
+    private static final Logger logger = LoggerFactory.getLogger(CrowdClientHolder.class);
+    private static CrowdClient client = initClient();
+    private static final String CROWD_PROPERTIES = "crowd.properties";
+
+    private CrowdClientHolder() {
+    }
+
+    private static CrowdClient initClient() {
+        CrowdClient result = null;
+        Properties crowdProperties = new Properties();
+        try {
+            File file = new File(JiveGlobals.getHomeDirectory() + File.separator + "conf" + File.separator + CROWD_PROPERTIES);
+            if (!file.exists() || !file.canRead()) {
+                throw new FileNotFoundException("Unable to open crowd.properties");
+            }
+            
+            crowdProperties.load(new FileInputStream(file));
+
+            String appName = crowdProperties.getProperty("application.name");
+            String appPass = crowdProperties.getProperty("application.password");
+            String url = crowdProperties.getProperty("crowd.server.url");
+
+            logger.info("Creating client to connect to:" + url);
+
+            result = new RestCrowdClientFactory().newInstance(url, appName, appPass);
+        } catch (IOException e) {
+            logger.error("ERROR creating Crowd client!!!!", e);
+        }
+
+        return result;
+    }
 
     public static CrowdClient getClient() {
         if (client == null) {
-            synchronized (CrowdClientHolder.class) {
-                if (client == null) {
-                    client = new RestCrowdClientFactory().newInstance("http://localhost:8095/crowd", "openfire", "password");
-                }
-            }
+            throw new IllegalStateException("Crowd client was unable to be instantiated!  Check earlier logs for errors.");
         }
         return client;
     }
